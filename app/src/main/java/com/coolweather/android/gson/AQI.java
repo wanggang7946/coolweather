a05dc82fdc202da6498159986478cc448533e6c4
wanggang7946 <wanggang7946@163.com>
2017-07-07 00:50:40 +0800

    添加天气信息

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017-07-06.
+ */
+
+public class AQI {
+    public AOICity city;
+    public class AOICity{
+        public String aqi;
+        public String pm25;
+    }
+}
